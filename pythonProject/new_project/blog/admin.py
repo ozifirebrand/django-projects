e85ac1e9d0765854b031604efae07d2f55d616bf
Ozioma <oziomaokoroafor@gmail.com>
2022-01-20 12:08:23 +0100

    Blog commit

--- pythonProject/new_project/blog/admin.py
+++ pythonProject/new_project/blog/admin.py
@@ -1,32 +1,20 @@
 from django.contrib import admin
-from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib import admin
+from .models import Post
+
+# admin.site.register(Post)
 
 
-class Post(models.Model):
-    STATUS_CHOICES = (
-        ("draft", "Draft"),
-        ("published", "Published")
-    )
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
 
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date="publish")
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name="blog_posts")
 
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,
-                              choices=STATUS_CHOICES,
-                              default="draft")
 
-    class Meta:
-        ordering = ("-publish",)
-
-        def __str__(self):
-            return self.title
 # Register your models here.
