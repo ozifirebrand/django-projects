7ce929dbd36373f95a1f1cf6c4334ec386b82b24
Ozioma <oziomaokoroafor@gmail.com>
2022-01-25 21:33:27 +0100

    Added Author and Post models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=45, null=False)
+    last_name = models.CharField(max_length=45, null=False)
+    username = models.CharField(max_length=50, null=False, unique=True)
+    password = models.CharField(max_length=32, null=False)
+    d_o_b = models.DateField
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 # Create your models here.
