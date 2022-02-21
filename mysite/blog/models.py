3a8ade85d06ed5b11868d6ca555d34c6ab878bbc
Ozioma <oziomaokoroafor@gmail.com>
2022-02-21 08:13:30 +0100

    Models.py commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,9 +17,19 @@ class Post(models.Model):
 
     author = models.ForeignKey(Author, related_name="blog_posts", on_delete=models.CASCADE)
 
-    publish_date = models.DateTimeField()
+    publish = models.DateTimeField()
 
     title = models.CharField(max_length=45, unique=False)
 
     slug = models.SlugField(max_length=100, unique_for_date=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
 # Create your models here.
